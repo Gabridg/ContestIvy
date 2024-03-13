28f13d1d40cdde4d6289e77e9096850e22b795a5
Gabridg <gabri99.del@gmail.com>
2024-03-13 15:11:11 +0100

    beta release 0.1

---
+++ script/config.js
@@ -0,0 +1,22 @@
+// var min = document.getElementById("min");
+// var max = document.getElementById("max");
+// var numberOfWinner = document.getElementById("number-of-winner");
+
+
+// axios.get("https://flynn.boolean.careers/exercises/api/array/integers?min="+ min + "&max=" + max + "&items=" + numberOfWinner).then(function(response){
+//     const winner = response.data;
+//     displayWinner(winner);
+
+//     function displayWinner(winner) {
+//         var resultElement = document.getElementById("result");
+//         resultElement.innerHTML = ""; // Pulisce il contenuto precedente
+//         winners.forEach(function(winner) {
+//             var winnerElement = document.createElement("h3");
+//             winnerElement.textContent = "Il vincitore del CONTEST è: " + winner;
+//             resultElement.appendChild(winnerElement);
+//         });
+//     }
+
+// });
+
+const apiKey = "66e0f1eb-b233-4f90-9a2c-c571e516625c";
